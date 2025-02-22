2014594d7846730bff54d8987e3c827b99247dcc
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-02-22 19:09:53 +0500

    Edit Performance Metric

--- server/routes/performanceMetricRoutes.js
+++ server/routes/performanceMetricRoutes.js
@@ -3,10 +3,13 @@ import { verifyToken } from './../middlewares/authorization.js';
 import { createPerformanceMetrics } from '../controllers/performanceMatricsControllers/createPerformanceMetric.js';
 import { assignMetric } from '../controllers/performanceMatricsControllers/assignMetric.js';
 import { getAllMetrics } from '../controllers/performanceMatricsControllers/getAllMetrics.js';
+import { editPerformanceMetric } from '../controllers/performanceMatricsControllers/editMetric.js';
 
 const router = express.Router();
 
 router.post('/create-metric', verifyToken, createPerformanceMetrics);
+
+router.put('/update-metric/:metric_id', verifyToken, editPerformanceMetric);
 
 router.post('/assign-metric', verifyToken, assignMetric);
 
