aca353064b0d105ef4b52791c81377d61758a4be
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-02-25 10:28:29 +0500

    Delete Parameter

--- server/routes/performenceParameterRoutes.js
+++ server/routes/performenceParameterRoutes.js
@@ -3,6 +3,7 @@ import { verifyToken } from "../middlewares/authorization.js";
 import { createPerformanceParameter } from "../controllers/performanceParameterControllers/createPerformanceParameter.js";
 import { updatePerformanceParameter } from "../controllers/performanceParameterControllers/editPerfomanceParameter.js";
 import { getPerformanceParametersByMetric } from "../controllers/performanceParameterControllers/getAllParameters.js";
+import { deletePerformanceParameter } from "../controllers/performanceParameterControllers/deleteParameter.js";
 
 const router = express.Router();
 
@@ -11,5 +12,7 @@ router.post('/create-parameter/:id', verifyToken, createPerformanceParameter);
 router.put('/edit-parameter/:parameter_id', verifyToken, updatePerformanceParameter);
 
 router.get('/get-all-parameters/:metric_id', verifyToken, getPerformanceParametersByMetric);
+
+router.delete('/delete-parameter/:id', verifyToken, deletePerformanceParameter);
 
 export default router;
