202314714e5b4b9e06634720aa0a9879adc7e2c7
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-02-25 09:58:44 +0500

    Get All Parameters based on Metric_Id

--- server/routes/performenceParameterRoutes.js
+++ server/routes/performenceParameterRoutes.js
@@ -2,11 +2,14 @@ import express from "express";
 import { verifyToken } from "../middlewares/authorization.js";
 import { createPerformanceParameter } from "../controllers/performanceParameterControllers/createPerformanceParameter.js";
 import { updatePerformanceParameter } from "../controllers/performanceParameterControllers/editPerfomanceParameter.js";
+import { getPerformanceParametersByMetric } from "../controllers/performanceParameterControllers/getAllParameters.js";
 
 const router = express.Router();
 
 router.post('/create-parameter/:id', verifyToken, createPerformanceParameter);
 
 router.put('/edit-parameter/:parameter_id', verifyToken, updatePerformanceParameter);
+
+router.get('/get-all-parameters/:metric_id', verifyToken, getPerformanceParametersByMetric);
 
 export default router;
