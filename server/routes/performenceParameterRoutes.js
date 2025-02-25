31ea6e62dc50633a2a803c915b8b7dab0878d9a1
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-02-25 09:42:08 +0500

    Edit Performance Parameter

--- server/routes/performenceParameterRoutes.js
+++ server/routes/performenceParameterRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middlewares/authorization.js";
 import { createPerformanceParameter } from "../controllers/performanceParameterControllers/createPerformanceParameter.js";
+import { updatePerformanceParameter } from "../controllers/performanceParameterControllers/editPerfomanceParameter.js";
 
 const router = express.Router();
 
 router.post('/create-parameter/:id', verifyToken, createPerformanceParameter);
+
+router.put('/edit-parameter/:parameter_id', verifyToken, updatePerformanceParameter);
 
 export default router;
