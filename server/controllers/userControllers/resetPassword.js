8a305dea51bb85ba450413e8a7b8ee86f3414b7f
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-02-25 21:49:27 +0500

    Update resetPassword.js

--- server/controllers/userControllers/resetPassword.js
+++ server/controllers/userControllers/resetPassword.js
@@ -1,16 +1,28 @@
 import { comparePassword, hashPassword } from '../../helpers/userHelpers.js';
 import db from '../../config/db.js';
+import jwt from 'jsonwebtoken';
 
 export const resetPassword = async (req, res) => {
     try {
 
-        const { token } = req.params;
+        let token = req.header('Authorization');
         const { oldPassword, newPassword } = req.body;
 
         if(!token){
             return res.status(400).send({
                 success: false,
-                message: "Token is missing"
+                message: "Token is missiing"
+            });
+        }
+
+        // Verify the token and extract the user ID
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user_id = decoded.id;
+
+        if (!user_id) {
+            return res.status(401).send({ 
+                success: false,
+                message: "Invalid token" 
             });
         }
 
@@ -22,9 +34,9 @@ export const resetPassword = async (req, res) => {
         }
 
         // Validate the token
-        const userQuery = `SELECT * FROM users WHERE reset_token = ? AND reset_token_expires > ?;`;
+        const userQuery = `SELECT * FROM users WHERE user_id = ? AND is_active = 1;`;
         const user = await new Promise((resolve, reject) => {
-            db.query(userQuery, [token, new Date()], (err, results) => {
+            db.query(userQuery, [user_id], (err, results) => {
                 if (err) return reject(err);
                 resolve(results[0]);
             });
@@ -33,7 +45,8 @@ export const resetPassword = async (req, res) => {
         if (!user) {
             return res.status(400).send({ 
                 success: false,
-                message: "Invalid or expired reset token" });
+                message: "Your data not found" 
+            });
         }
 
         // Verify the old password
@@ -49,9 +62,9 @@ export const resetPassword = async (req, res) => {
         const hashedPassword = await hashPassword(newPassword);
 
         // Update the user's password and clear the reset token
-        const updateQuery = `UPDATE users SET password = ?, reset_token = NULL, reset_token_expires = NULL WHERE reset_token = ?;`;
+        const updateQuery = `UPDATE users SET password = ? WHERE user_id = ?;`;
         await new Promise((resolve, reject) => {
-            db.query(updateQuery, [hashedPassword, token], (err, results) => {
+            db.query(updateQuery, [hashedPassword, user_id], (err, results) => {
                 if (err) return reject(err);
                 resolve(results);
             });
