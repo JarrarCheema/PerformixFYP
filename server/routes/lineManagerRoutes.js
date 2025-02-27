ac2c226c8149a3a0678f2379d027603561cc2640
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-02-27 07:08:00 +0500

    View Line Manager Dashboard

--- server/routes/lineManagerRoutes.js
+++ server/routes/lineManagerRoutes.js
@@ -2,12 +2,15 @@ import express from 'express';
 import { verifyToken } from '../middlewares/authorization.js';
 import { getEmployees } from '../controllers/lineManagerControllers/getEmployees.js';
 import { getEmployeeMetrics } from '../controllers/lineManagerControllers/getEmployeeMetrics.js';
+import { viewLineManagerDashboard } from '../controllers/lineManagerControllers/viewLineManagerDashboard.js';
 
 const router = express.Router();
 
 router.get('/get-employees', verifyToken, getEmployees);
 
 router.get('/get-employee-metrics/:user_id', verifyToken, getEmployeeMetrics);
+
+router.get('/view-lm-dashboard', verifyToken, viewLineManagerDashboard);
 
 
 export default router;
