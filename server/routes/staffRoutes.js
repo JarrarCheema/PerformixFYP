c2fc8c03e60ab9481d8c315801c7e0ba1263a5b9
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-02-28 03:42:36 +0500

    Added View Staff Leaderboard

--- server/routes/staffRoutes.js
+++ server/routes/staffRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../middlewares/authorization.js';
 import { viewStaffDashboard } from '../controllers/staffControllers/viewStaffDashboard.js';
+import { viewStaffLeaderboard } from '../controllers/staffControllers/viewStaffLeaderboard.js';
 
 const router = express.Router();
 
 router.get('/view-staff-dashboard', verifyToken, viewStaffDashboard);
+
+router.get('/view-staff-leaderboard', verifyToken, viewStaffLeaderboard);
 
 
 
