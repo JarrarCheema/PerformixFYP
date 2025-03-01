355970b7f43209214a066a686420d933aabfcf17
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-03-02 04:00:23 +0500

    Get Employee submitted survey response by Admin

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -6,19 +6,22 @@ import { getSingleSurveyDetails } from '../controllers/surveyControllers/getSing
 import { submitSurveyResponse } from '../controllers/surveyControllers/submitSurveyResponse.js';
 import { getAdminSurveys } from '../controllers/surveyControllers/getAdminSurveys.js';
 import { getAdminSingleSurvey } from '../controllers/surveyControllers/getAdminSingleSurvey.js';
+import { getSurveyResponse } from '../controllers/surveyControllers/getSurveyResponse.js';
 
 const router = express.Router();
 
 router.post('/create-survey/:organization_id', verifyToken, createSurvey);
-
-router.get('/get-admin-surveys/:organization_id', verifyToken, getAdminSurveys);
-
-router.get('/get-admin-survey/:survey_id', verifyToken, getAdminSingleSurvey);
 
 router.get('/get-surveys', verifyToken, getSurveysForEmployee);
 
 router.get('/get-survey-details/:survey_id', verifyToken, getSingleSurveyDetails);
 
 router.post('/submit-survey/:survey_id', verifyToken, submitSurveyResponse);
+
+router.get('/get-admin-surveys/:organization_id', verifyToken, getAdminSurveys);
+
+router.get('/get-admin-survey/:survey_id', verifyToken, getAdminSingleSurvey);
+
+router.get('/get-survey-response/:survey_id/:user_id', verifyToken, getSurveyResponse);
 
 export default router;
