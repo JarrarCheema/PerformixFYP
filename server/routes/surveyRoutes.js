ba8a67f4ecc652fb41415f4e6d99c1680f694640
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-03-02 03:05:55 +0500

    Get all the created surveys by Admin

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -4,10 +4,13 @@ import { createSurvey } from '../controllers/surveyControllers/createSurvery.js'
 import { getSurveysForEmployee } from '../controllers/surveyControllers/getSurveyForEmployees.js';
 import { getSingleSurveyDetails } from '../controllers/surveyControllers/getSingleSurveyDetails.js';
 import { submitSurveyResponse } from '../controllers/surveyControllers/submitSurveyResponse.js';
+import { getAdminSurveys } from '../controllers/surveyControllers/getAdminSurveys.js';
 
 const router = express.Router();
 
 router.post('/create-survey/:organization_id', verifyToken, createSurvey);
+
+router.get('/get-admin-surveys/:organization_id', verifyToken, getAdminSurveys);
 
 router.get('/get-surveys', verifyToken, getSurveysForEmployee);
 
