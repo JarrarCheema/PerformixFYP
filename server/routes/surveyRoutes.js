462ec63f6554bdd06caebbc814b33c229ecb2b6e
Daniyal Zafar <malikdaniyalzafar@gmail.com>
2025-03-02 01:25:03 +0500

    Get All Surveys by Employees

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../middlewares/authorization.js';
 import { createSurvey } from '../controllers/surveyControllers/createSurvery.js';
+import { getSurveysForEmployee } from '../controllers/surveyControllers/getSurveyForEmployees.js';
 
 const router = express.Router();
 
 router.post('/create-survey/:organization_id', verifyToken, createSurvey);
+
+router.get('/get-surveys', verifyToken, getSurveysForEmployee);
 
 export default router;
